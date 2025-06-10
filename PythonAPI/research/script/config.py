3b10ad0e2ae7becd8bcaa8c5556d6c0468f2b64d
Nagao Shunsuke <gcycr0310@gmail.com>
2025-06-10 20:27:33 +0900

    リファクタリング：定数を別のファイルに移動

---
+++ PythonAPI/research/script/config.py
@@ -0,0 +1,26 @@
+import carla
+CARLA_HOST = 'localhost'
+CARLA_PORT = 2000
+TIMEOUT = 10
+
+MAP = "Town01_Opt"
+TIME_DURATION = 1000 # シミュレーション時間（秒）
+VALID_DISTANCE = 50 # バウンディングボックス検出の最大距離
+FIXED_DELTA_SECONDS = 0.1 # シミュレーションのステップ時間
+
+CAR_RATIO = 0.5 # NPC車両のスポーン割合
+
+IM_WIDTH = 800
+IM_HEIGHT = 600
+FOV = 60
+
+OUTPUT_IMG_DIR = "C:\\CARLA_Latest\\WindowsNoEditor\\output\\image"
+OUTPUT_LABEL_DIR = "C:\\CARLA_Latest\\WindowsNoEditor\\output\\label"
+
+# YOLOv5 COCOデータセット準拠のクラスIDを想定
+CLASS_MAPPING = {
+    carla.CityObjectLabel.TrafficLight: 9,   # COCO: traffic light
+    carla.CityObjectLabel.TrafficSigns: 11,  # COCO: stop sign (traffic signをstop signとして扱う)
+    carla.CityObjectLabel.Vehicles: 2,       # COCO: car
+    carla.CityObjectLabel.Pedestrians: 0,    # COCO: person
+}
--- PythonAPI/research/script/spawn_npcs_and_record.py
+++ PythonAPI/research/script/spawn_npcs_and_record.py
@@ -7,7 +7,6 @@ import time
 import queue
 import numpy as np
 import cv2
-import datetime
 
 # === Carla Egg のパス設定 ===
 try:
@@ -19,30 +18,8 @@ except IndexError:
     pass
 
 # === グローバル変数の設定 ===
-CARLA_HOST = 'localhost'
-CARLA_PORT = 2000
-
-MAP = "Town01_Opt"
-TIME_DURATION = 1000 # シミュレーション時間（秒）
-VALID_DISTANCE = 50 # バウンディングボックス検出の最大距離
-FIXED_DELTA_SECONDS = 0.1 # シミュレーションのステップ時間
-
-CAR_RATIO = 0.5 # NPC車両のスポーン割合
-
-IM_WIDTH = 800
-IM_HEIGHT = 600
-FOV = 60
-
-OUTPUT_IMG_DIR = "C:\\CARLA_Latest\\WindowsNoEditor\\output\\image"
-OUTPUT_LABEL_DIR = "C:\\CARLA_Latest\\WindowsNoEditor\\output\\label"
-
-# YOLOv5 COCOデータセット準拠のクラスIDを想定
-CLASS_MAPPING = {
-    carla.CityObjectLabel.TrafficLight: 9,   # COCO: traffic light
-    carla.CityObjectLabel.TrafficSigns: 11,  # COCO: stop sign (traffic signをstop signとして扱う)
-    carla.CityObjectLabel.Vehicles: 2,       # COCO: car
-    carla.CityObjectLabel.Pedestrians: 0,    # COCO: person
-}
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from config import *
 
 # === ヘルパー関数 ===
 def build_projection_matrix(w, h, fov, is_behind_camera=False):
@@ -232,10 +209,9 @@ def process_camera_data(image, camera_actor, world, K, K_b, ego_vehicle, output_
 
 # === Carlaサーバに接続 ===
 client = carla.Client(CARLA_HOST, CARLA_PORT)
-client.set_timeout(10.0)
+client.set_timeout(TIMEOUT)
 
 # マップ変更
-print(client.get_available_maps())
 client.load_world(MAP)
 world = client.get_world()
 blueprint_library = world.get_blueprint_library()

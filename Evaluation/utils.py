d5c043c4a950e6a9b4bdbe1fc7b94e1fff00389f
Nagao Shunsuke <gcycr0310@gmail.com>
2025-09-19 17:56:52 +0900

    change:共通する処理をutilに分離

---
+++ Evaluation/utils.py
@@ -0,0 +1,27 @@
+SIZE_THRESHOLD = 200  # バウンディングボックスの最小サイズ
+IoU_THRESHOLD = 0.5  # IoUの閾値
+class_Map = {
+    0: 0,  # pedestrian
+    1: 2,  # bicycle
+    2: 2,  # motorcycle
+    3: 2,  # car
+    5: 2,  # bus
+    7: 2,  # truck
+    9: 9,  # traffic light
+    # 11: 11, #stop sign
+}
+
+
+def iou(box1, box2):
+    axmin, axmax, aymin, aymax, _ = box1
+    bxmin, bxmax, bymin, bymax, _ = box2
+    area_a = (axmax - axmin) * (aymax - aymin)
+    area_b = (bxmax - bxmin) * (bymax - bymin)
+
+    abxmin = max(axmin, bxmin)
+    abxmax = min(axmax, bxmax)
+    abymin = max(aymin, bymin)
+    abymax = min(aymax, bymax)
+    intersection = max(0, abxmax - abxmin) * max(0, abymax - abymin)
+    union = area_a + area_b - intersection
+    return intersection / union if union > 0 else 0

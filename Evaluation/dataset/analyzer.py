c086c9e5cfea8136f5ccdd3a9810ce6bbd38a643
Nagao Shunsuke <gcycr0310@gmail.com>
2025-11-01 16:20:29 +0900

    intersectionとunionを計算するところまで

--- Evaluation/dataset/analyzer.py
+++ Evaluation/dataset/analyzer.py
@@ -5,13 +5,76 @@ class DetectionAnalyzer:
     def __init__(self, iou_th: float):
         self.iou_th = iou_th
 
-    def analyze_frame(self, gt_path: str, det_paths: list[str]) -> dict:
-        frame_results = dict()
+    def analyze_frame(self, gt: dict, dets: dict[dict]) -> dict:
+        classified_results = self._classify_frame(
+            gt, dets)  # これでdetsのバージョン数が切り替わっても使える
+        intersection_errors = self._intersection_of_errors(classified_results)
+        union_errors = self._union_of_errors(classified_results)
+        return {
+            'classified_results': classified_results,
+            'intersection_errors': intersection_errors,
+            'union_errors': union_errors
+        }
+
+    def _intersection_of_errors(self, classified_dets) -> dict:
+        intersection_errors = {'FP': dict(), 'FN': dict()}
+        for error_type in ['FP', 'FN']:
+            for version, classified_det in classified_dets.items():
+                if version == 0:
+                    base_errors = classified_det[error_type]
+                    for class_id in base_errors.keys():
+                        intersection_errors[error_type][class_id] = list()
+                        for boxes in base_errors[class_id]:
+                            intersection_errors[error_type][class_id].append(
+                                boxes)
+                else:
+                    current_errors = classified_det[error_type]
+                    used_boxes = set()
+                    for class_id in intersection_errors[error_type].keys():
+                        if class_id not in current_errors:
+                            del intersection_errors[error_type][class_id]
+                            continue
+                        for base_box in intersection_errors[error_type][class_id]:
+                            best_iou = 0.0
+                            for curr_box in current_errors[class_id]:
+                                if curr_box in used_boxes:
+                                    continue
+                                print("base_box", base_box)
+                                print("curr_box", curr_box)
+                                iou = utils.iou(base_box, curr_box)
+                                if iou > best_iou:
+                                    best_iou = iou
+                            if best_iou >= self.iou_th:
+                                used_boxes.add(curr_box)
+                            else:
+                                intersection_errors[error_type][class_id].remove(
+                                    base_box)
+            return intersection_errors
+
+    def _union_of_errors(self, dets: dict[dict]) -> dict:
+        union_errors = {'FP': dict(), 'FN': dict()}
+        for error_type in ['FP', 'FN']:
+            for version, classified_det in dets.items():
+                current_errors = classified_det[error_type]
+                for class_id in current_errors.keys():
+                    if class_id not in union_errors[error_type]:
+                        union_errors[error_type][class_id] = list()
+                    for box in current_errors[class_id]:
+                        # すでに追加されているか確認
+                        already_added = False
+                        for existing_box in union_errors[error_type][class_id]:
+                            iou = utils.iou(box, existing_box)
+                            if iou >= self.iou_th:
+                                already_added = True
+                                break
+                        if not already_added:
+                            union_errors[error_type][class_id].append(box)
+        return union_errors
 
     def _classify_frame(self, gt, dets) -> dict[dict]:
         frame_results = dict()
         # 各バージョンの検出結果を分類
-        for version, det in enumerate(dets):
+        for version, det in dets.items():
             frame_results[version] = self._classify(gt, det)
         return frame_results
 

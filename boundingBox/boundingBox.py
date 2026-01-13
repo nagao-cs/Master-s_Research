05f41bf3808374c90b0a4febdf9c1608196c23d3
Nagao Shunsuke <gcycr0310@gmail.com>
2026-01-13 17:00:17 +0900

    change labelは使わないので削除

--- boundingBox/boundingBox.py
+++ boundingBox/boundingBox.py
@@ -44,13 +44,12 @@ class BoundingBox(ABC):
 
 
 class DetectionBoundingBox(BoundingBox):
-    def __init__(self, xCenter: float, yCenter: float, width: float, height: float, classId: int, label: str, confidenceScore: float):
+    def __init__(self, xCenter: float, yCenter: float, width: float, height: float, classId: int, confidenceScore: float):
         self.xCenter = xCenter
         self.yCenter = yCenter
         self.width = width
         self.height = height
         self.classId = classId
-        self.label = label
         self.confidenceScore = confidenceScore
 
 
@@ -71,13 +70,12 @@ class ClassifyCategory(Enum):
 
 
 class ClassifiedBoundingBox(BoundingBox):
-    def __init__(self, xCenter: float, yCenter: float, width: float, height: float, classId: int, label: str, confidenceScore: Optional[float], classifyCategory: ClassifyCategory):
+    def __init__(self, xCenter: float, yCenter: float, width: float, height: float, classId: int, confidenceScore: Optional[float], classifyCategory: ClassifyCategory):
         self.xCenter = xCenter
         self.yCenter = yCenter
         self.width = width
         self.height = height
         self.classId = classId
-        self.label = label
         if confidenceScore == None:
             self.confidenceScore = 0.0
         else:

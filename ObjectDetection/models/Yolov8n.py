5a3da293158be82f3700d2198bf6b85e4b5a8525
Nagao Shunsuke <gcycr0310@gmail.com>
2026-01-13 14:18:23 +0900

    バウンディングボックスをインスタンス化しないでタプルで処理した場合

--- ObjectDetection/models/Yolov8n.py
+++ ObjectDetection/models/Yolov8n.py
@@ -3,11 +3,15 @@ import cv2
 from .AbstractObjectDetector import AbstractObjectDetector
 from ..utils import utils
 from boundingBox.boundingBox import DetectionBoundingBox
+import torch
 
 
 class Yolov8nDetector(AbstractObjectDetector):
     def __init__(self):
         self.model = None
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"device is {self.device}")
+        # self.device = "cpu"
         self.load_model()
 
     def load_model(self):
@@ -25,7 +29,8 @@ class Yolov8nDetector(AbstractObjectDetector):
 
         imageWidth = image.shape[1]
         imageHeight = image.shape[0]
-        detections = self.model.predict(image, device="cuda")
+
+        detections = self.model.predict(image, device=self.device)
         rawBoundingBoxList = detections[0].boxes
 
         outputBoundingBoxList = list()
@@ -44,13 +49,16 @@ class Yolov8nDetector(AbstractObjectDetector):
             yCenter = (ymin + ymax) / 2
             width = xmax - xmin
             height = ymax - ymin
-            classId = int(rawBoundingBox.cls[0])
+            classId = utils.COCO_ID_MAPPER.get(int(rawBoundingBox.cls[0]), -1)
+            if classId not in utils.VALID_CLASS_ID:
+                continue
+            label = utils.COCO_LABELS.get(classId, 'unknown')
             confidencescore = rawBoundingBox.conf[0].item()
-            label = self.model.names[classId] if classId < len(
-                self.model.names) else 'unknown'
 
-            boundingBoxInstance = DetectionBoundingBox(
+            boundingBoxInstance = (
                 xCenter, yCenter, width, height, classId, label, confidencescore)
+            # boundingBoxInstance = DetectionBoundingBox(
+            # xCenter, yCenter, width, height, classId, label, confidencescore)
             outputBoundingBoxList.append(boundingBoxInstance)
 
         return outputBoundingBoxList

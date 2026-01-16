d0e9def3cbf23d9bf1ce899809b5631f60bb77f7
Nagao Shunsuke <gcycr0310@gmail.com>
2026-01-16 18:19:32 +0900

    add 速度や各バージョンでの推論回数を追加

--- adaptiveNversion/statsRecorder.py
+++ adaptiveNversion/statsRecorder.py
@@ -11,29 +11,37 @@ class StatsRecorder:
         self.numTransitionFromOneToN: int = 0
         self.numTransitionFromNToOne: int = 0
         self.totalExecutionTime: float = 0.0
+        self.throuput: float = 0.0
+        self.numProcessedImage: int = 0
+        self.numOneVersionDetectionCount: int = 0
+        self.numNVersionDetectionCount: int = 0
         self._previousState: VersionState = VersionState.ONE
         self._maxVersion = len(modelNameList)
 
     def update(self, versionsState: VersionState):
         if versionsState == VersionState.ONE:
             self.numInference += 1
+            self.numOneVersionDetectionCount += 1
             if self._previousState == VersionState.N:
                 self.numTransitionFromNToOne += 1
         elif versionsState == VersionState.N:
             self.numInference += self._maxVersion
+            self.numNVersionDetectionCount += 1
             if self._previousState == VersionState.ONE:
                 self.numTransitionFromOneToN += 1
 
         self._previousState = versionsState
+        self.numProcessedImage += 1
 
     def registerExecutionTime(self, executionTime: float):
         self.totalExecutionTime = executionTime
+        self.throuput = self.numProcessedImage / self.totalExecutionTime
 
     def writeStatsToCsvFile(self, statsWriteCsvFilePath: Path):
-        header: list[str] = ['model', 'num Inferece', 'num Transition from One to N',
-                             'num Transition from N to One', 'execution Time']
-        writeContent: list = [self.model, self.numInference, self.numTransitionFromOneToN,
-                              self.numTransitionFromNToOne, self.totalExecutionTime]
+        header: list[str] = ['model', 'num Inferece', 'num Oneversion Detection', 'num N-Version Detection', 'num Transition from One to N',
+                             'num Transition from N to One', 'execution Time', 'throuput']
+        writeContent: list = [self.model, self.numInference, self.numOneVersionDetectionCount, self.numNVersionDetectionCount, self.numTransitionFromOneToN,
+                              self.numTransitionFromNToOne, self.totalExecutionTime, self.throuput]
         if not os.path.exists(statsWriteCsvFilePath):
             with open(statsWriteCsvFilePath, mode='w') as csvFile:
                 writer = csv.writer(csvFile)
